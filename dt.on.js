1669c1dfa15edad149991821600aef657115764d
Parky <parky_18@163.com>
2016-07-01 14:12:44 +0800

    update to v1.0.9

--- dt.on.js
+++ dt.on.js
@@ -10,6 +10,7 @@
         for (var i = 0; i < attrs.length; i++) {
             var pairs = attrs[i].split("=");
             if (pairs.length === 2) {
+
                 ctx.options.$currentElement.on($.dt.trim(pairs[0]),$.dt.template.compile($.dt.trim(pairs[1]), ctx.options.data));
             }
         }
